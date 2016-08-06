ff61077039ddded5ff49830b32aef8f63d56de72
kaysh <kaysh@KAYSH-LAPTOP>
2016-08-06 13:40:11 +0200

    Added google maps :)

---
+++ MedicalAssistant/Scripts/app.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+
+    var apiKey = "AIzaSyAg1OxEX_KREPBl0l9LhxtT867amfl3gFE";
+    var query = "Gateway,Durban,South+Africa";
+
+    $("#googleMapiFrame").attr("src", "https://www.google.com/maps/embed/v1/place?key=" + apiKey + "&q=" + query);
+});
